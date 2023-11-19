b89205fcb522f3c231131f138c6ff20ce87f428b
Spirit-Q <85896487+Spirit-Q@users.noreply.github.com>
2023-11-19 16:11:34 +0300

    Update script.user.js

--- misc/script.user.js
+++ misc/script.user.js
@@ -17,7 +17,7 @@
         return new RegExp("^" + rule.split("*").map(escapeRegex).join(".*") + "$").test(str);
     }
 
-    var root = "https://cdn.jsdelivr.net/gh/theairblow/hackpack/uchihack/";
+    var root = "https://cdn.jsdelivr.net/gh/Spirit-Q/uchihackkingda";
     var rawConfig = await fetch(root + "injector/config.json");
     var config = await rawConfig.json();
 
